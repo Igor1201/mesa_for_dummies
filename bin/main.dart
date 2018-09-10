a0fbc415afc19dd07d9934b259ee6a0ee7745006
Igor Borges <igor@borges.me>
2018-09-10 20:40:53 -0300

    first commit, from stagehand

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:mesa_for_dummies/mesa_for_dummies.dart' as mesa_for_dummies;
+
+main(List<String> arguments) {
+  print('Hello world: ${mesa_for_dummies.calculate()}!');
+}
---
+++ test/mesa_for_dummies_test.dart
@@ -0,0 +1,8 @@
+import 'package:mesa_for_dummies/mesa_for_dummies.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
